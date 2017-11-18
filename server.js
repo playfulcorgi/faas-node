46f718bebe46dcec36c4af0595a896cf04fb3f57
Tomasz Niezgoda <tomasz.niezgoda@mail.com>
2017-11-18 16:55:52 +0100

    no message

--- server.js
+++ server.js
@@ -8,6 +8,8 @@
 // https://www.npmjs.com/package/body-parser
 // https://github.com/openfaas/faas/blob/master/sample-functions/NodeInfo/main.js
 // https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
+// https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
+// https://www.npmjs.com/package/qs
 
 const connect = require('connect')
 const http = require('http')
@@ -16,23 +18,29 @@ const bodyParser = require('body-parser')
 const serverPort = 80
 const morgan = require('morgan')
 const responseTime = require('response-time')
-const cookieParser = require('cookie-parser')
+const qs = require('qs')
+const url = require('url');
+const send = require('connect-send-json')
 
 app.use(responseTime())
 app.use(morgan('combined'))
-// Maybe I'm using too many parsers (cookie, body). Maybe parsing a request should be up to the handler instead to save resources, so the handler would only parse what he needs.
-app.use(cookieParser())
+// Maybe I'm using too many parsers (bodyParser). Maybe parsing a request should be up to the handler instead to save resources, so the handler would only parse what he needs.
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(bodyParser.raw())
 app.use(bodyParser.text())
+app.use(send.json())
 // TODO: replace console.log with logging to a better logger.
-// TODO: parse querystring automatically
-app.use((req, res) => {
-	console.log(req.url)
-	console.log()
-  res.end('Hello from Connect!\n')
+app.use((request, response) => {
+	const urlParts = url.parse(request.url, true)
+	request.query = urlParts.query
+	response.setHeader('Content-Type', 'text/plain')
+	// console.log(request.query)
+	// response.end('Hello from Connect!\n')
+	// response.json({
+	// 	someKey: 'someValue of key'
+	// })
 })
 const server = http.createServer(app)
-server.setTimeout(20000)// The server will stop any stuck or long request handlers.
+server.setTimeout(10000)// The server will stop any stuck or long request handlers.
 server.listen(serverPort)
