94028e7dd7f39268ef76c979ce9b7b0fc0faa823
Tomasz Niezgoda <tomasz.niezgoda@mail.com>
2017-11-18 20:24:51 +0100

    no message

--- default-index.js
+++ default-index.js
@@ -1,8 +1,5 @@
 exports.handler = (request, response) => {
-	// response.json({
-	// 	message: 'This is a sample response.'
-	// })
-	setTimeout(() => {
-		throw new Error('some error')
-	}, 500)
+	response.json({
+		message: 'This is a sample response.'
+	})
 }
